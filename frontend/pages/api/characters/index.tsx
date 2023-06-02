130ff825b8629c80670c7bc356e33ca9c44ade52
Marissa Epstein <marissa.epstein@condenast.com>
2023-06-02 18:30:03 +0100

    added testing and read me

--- frontend/pages/api/characters/index.tsx
+++ frontend/pages/api/characters/index.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default async function getRickAndMorty(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { data } = await axios.get('http://localhost:8000/v1/characters');
+    const { data } = await axios.get(`http://${process.env.BACKEND_API_URL}/v1/characters`);
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err });
